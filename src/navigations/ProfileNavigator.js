0ddc4593138d6c379449d5eca4a251a451cf99a2
ArunKarthik2197 <arun.munipalle1997@gmail.com>
2020-10-16 19:25:49 -0700

    Profile navigator -- signout

--- src/navigations/ProfileNavigator.js
+++ src/navigations/ProfileNavigator.js
@@ -1,20 +1,38 @@
 /**
  * ProfileNavigator - main navigator which holds entire app navigation.
  */
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React,{ useContext } from 'react';
+import { createDrawerNavigator, DrawerItem, DrawerItemList, DrawerContentScrollView } from '@react-navigation/drawer';
 
 import MyProfileScreen from '../screens/user-screens/MyProfileScreen';
 import SettingsScreen from '../screens/user-screens/SettingsScreen';
 import AppTabsNavigator from '../navigations/AppNavigator';
+import { AuthContext } from "./AuthContext";
+
+// import { CButton } from "../components/atoms"
 
 const ProfileDrawer = createDrawerNavigator();
 
 export default function ProfileDrawerNavigator() {
+    const { signOut } = useContext(AuthContext)
     return(
-        <ProfileDrawer.Navigator initialRouteName="App">
+        <ProfileDrawer.Navigator initialRouteName="App" drawerContent = {
+            (props) => {return (
+            <DrawerContentScrollView {...props}>
+                <DrawerItemList {...props}/>
+                <DrawerItem label = "Sign Out" style = {{backgroundColor:"tomato"}} labelStyle = {{color:"#fffeee",fontSize:20}} onPress = {signOut}/>
+            </DrawerContentScrollView>
+            ) }          
+        }
+        drawerContentOptions = {
+            {
+                activeTintColor:"#e95021",
+                labelStyle: {fontSize:20}
+            }
+        }
+        >
             <ProfileDrawer.Screen name="App" component={AppTabsNavigator} />
-            <ProfileDrawer.Screen name="MyProfile" component={MyProfileScreen} />
+            <ProfileDrawer.Screen name="My Profile" component={MyProfileScreen} />
             <ProfileDrawer.Screen name="Settings" component={SettingsScreen} />
         </ProfileDrawer.Navigator>
     )
